d589607fabb244331f28a266da65add4be498ddb
xiaogao <G805370101@163.com>
2023-06-02 10:34:13 +0800

    dddddddd

--- student_manager/src/com/xxxy/studentmanager/main/ActionStudentManagement.java
+++
@@ -1,9 +0,0 @@
-package com.xxxy.studentmanager.main;
-
-import com.xxxy.studentmanager.views.LoginGUI;
-
-public class ActionStudentManagement {
-    public static void main(String[] args) {
-        new LoginGUI();
-    }
-}
